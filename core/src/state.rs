1ca905ac8a49cd4ae794b3a9c27f8703b7346128
Guillaume W. Bres <guillaume.bressaix@gmail.com>
2025-10-05 12:56:20 +0200

    update
    Signed-off-by: Guillaume W. Bres <guillaume.bressaix@gmail.com>

--- core/src/state.rs
+++ core/src/state.rs
@@ -1,6 +1,9 @@
-use crate::prelude::{Duration, Epoch};
+use crate::prelude::{Duration, Epoch, PhysicsResult};
 
-/// Modeled and predictable [State]s
+#[cfg(doc)]
+use crate::prelude::TimeScale;
+
+/// [State] model
 pub trait State: Copy + Clone {
     /// Generates a default yet physically correct [State].
     fn default(epoch: Epoch) -> Self;
@@ -11,17 +14,33 @@ pub trait State: Copy + Clone {
     /// Returns current [Epoch] of this [State]
     fn epoch(&self) -> Epoch;
 
-    /// Returns true when this [State] does not result from an actual observation.
+    /// Provide a new [State] observation.
+    fn observed(self, state: Self) -> Self;
+
+    /// Observe a new [State].
+    fn observe_mut(&mut self, state: Self) {
+        *self = self.observed(state);
+    }
+}
+
+/// [Predictable] [State]s
+pub trait Predictable: State {
+    /// Returns true if this [Predictable] is currently compatible with a new prediction.
+    /// Which might not be the case, in particular when the pending update is not complete.
+    fn predictable(&self) -> bool;
+
+    /// Returns true if this latest [Predictable] state was actually predicted or not.
+    /// Which is not necesarilly the case, it may come from actual measurement.
     fn predicted(&self) -> bool;
 
-    /// Predict a new [S]tate either forward or backwards, of given step [Duration].
-    fn predict(self, step: Duration) -> Self;
+    /// Predict this [Predictable] using last measurement and current state, of given step [Duration].
+    /// Use negative [Duration]s for backwards prediction.
+    fn predict(self, step: Duration) -> PhysicsResult<Self>;
 
-    /// Observe a new [S]tate at specific [Epoch]
-    fn observe(&mut self, state: Self);
-
-    /// Moves to new [S]tate either forward or backwards, of given step [Duration].
-    fn predict_mut(&mut self, step: Duration) {
-        *self = self.predict(step);
+    /// Advance this mutable [Predictable] state using current state, either forward
+    /// or backwards when using negative [Duration].
+    fn predict_mut(&mut self, step: Duration) -> PhysicsResult<()> {
+        *self = self.predict(step)?;
+        Ok(())
     }
 }
